f5b291ef093c3988b11059b1f6ea3a689fa9fd89
Fernando <fernandomenjivar2011@gmail.com>
2019-05-29 13:07:12 -0600

    🎉 first commit

---
+++ InnguzApp/Controllers/UsuariosController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using InnguzApp.ContextoDatos;
+
+namespace InnguzApp.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        Base_DatosDataContext bd = new Base_DatosDataContext();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            IEnumerable<Usuarios> Lista = (from u in bd.Usuarios select u).ToList();
+            return View(Lista);
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Usuarios modelo)
+        {
+            try
+            {
+                bd.Usuarios.InsertOnSubmit(modelo);
+                bd.SubmitChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ InnguzApp/Scripts/main.js
@@ -0,0 +1,36 @@
+﻿var burguer = document.querySelector('.hamburger');
+var menuWrapper = document.querySelector('.nav-container');
+var menuItems = document.querySelectorAll('.menu-item a');
+var menuItemsArray = Array.prototype.slice.call(menuItems);
+var urlArr = window.location.href.split('/');
+var urlArgument = urlArr[urlArr.length - 1];
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 5 && window.innerWidth < 768) {
+        menuWrapper.classList.remove('fixed');
+        menuWrapper.classList.add('fixed');
+    } else {
+        menuWrapper.classList.remove('fixed');
+    }
+})
+
+if (window.innerWidth >= 768) {
+    menuWrapper.classList.remove('fixed');
+}
+
+burguer.addEventListener('click', function () {
+    burguer.classList.toggle('is-active');
+    var mobileMenu = document.querySelector('.mobile-nav');
+    mobileMenu.classList.toggle('active');
+
+    mobileMenu.classList.contains('active') ? menuWrapper.classList.add('fixed') : menuWrapper.classList.remove('fixed');
+})
+
+menuItemsArray.forEach(function (item, index) {
+    var itemUrl = item.href.split('/');
+    var itemUrlArg = itemUrl[itemUrl.length - 1];
+    if (itemUrlArg === urlArgument) {
+        item.classList.add('active');
+    }
+});
+
